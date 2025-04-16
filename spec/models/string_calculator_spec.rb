fcdd1db831499faf8b650cc7cd181395446627f2
mukeshkumar1921 <mukesh.kumar1@dotsquares.com>
2025-04-16 12:06:15 +0530

    Give value like 5

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe StringCalculator, type: :model do
       result = StringCalculator.add('')
       expect(result).to eq(0)
     end
+	
+	it 'returns the number itself for a single number' do
+      result = StringCalculator.add('5')
+      expect(result).to eq(5)
+    end
+
   end
 end
