f24b337b3e6301ee5ae3fd1a94b40f828e16d2c8
mukeshkumar1921 <mukesh.kumar1@dotsquares.com>
2025-04-16 12:22:12 +0530

    Update the add method to handle custom delimiters

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -2,6 +2,15 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/[\n,]/).map(&:to_i).sum
+    if numbers.start_with?('//')
+      delimiter_end = numbers.index("\n")
+      delimiter = numbers[2..delimiter_end - 1]
+      numbers = numbers[delimiter_end + 1..-1]
+      numbers = numbers.split(delimiter)
+    else
+      numbers = numbers.split(/[\n,]/)
+    end
+
+    numbers.map(&:to_i).sum
   end
 end
